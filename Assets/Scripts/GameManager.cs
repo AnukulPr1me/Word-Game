4a3f5ce25662c06cd05b6932677c6aab6f9540b6
Anukul <101039186+AnukulPr1me@users.noreply.github.com>
2023-07-10 23:34:28 +0530

    push 4.5
    UI elements

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum GameState { Menu, Game, LevelComplete, GameOver, Idle}
+
+public class GameManager : MonoBehaviour
+{
+
+    public static GameManager instance;
+
+    [Header("Settings")]
+    private GameState gameState;
+
+    [Header("Events")]
+    public static Action<GameState> onGameStateChanged;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+
+    }
+
+    public void SetGameState(GameState gameState)
+    {
+        this.gameState = gameState;
+        onGameStateChanged?.Invoke(gameState);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
